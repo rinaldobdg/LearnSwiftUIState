d5e9e316cfcea9ba387a5211176eca6388e518e3
Benny Rinaldo <rinaldobdg@yahoo.com>
2026-04-17 10:40:24 +0700

    Initial Commit

---
+++ LearnSwiftUIState/LearnSwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIStateApp.swift
+//  LearnSwiftUIState
+//
+//  Created by Rinaldo on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
