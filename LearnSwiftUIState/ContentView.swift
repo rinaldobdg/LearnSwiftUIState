6046a211c617a57344b2c8cfc85729514946662b
Benny Rinaldo <rinaldobdg@yahoo.com>
2026-04-17 11:32:40 +0700

    My learning journey on Swift / SwiftUI

--- LearnSwiftUIState/ContentView.swift
+++ LearnSwiftUIState/ContentView.swift
@@ -8,19 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPlaying = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button {
+            // switch between the play and stop button
+            isPlaying.toggle()
+        } label: {
+            Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
+                .font(.system(size: 150))
+                .foregroundColor(isPlaying ? .red : .green)
         }
-        .padding()
+    }
+}
+
+struct CounterView: View {
+    @State private var counter = 1
+    
+    var body: some View {
+        Button {
+            // switch between the play and stop button
+            counter += 1
+        } label: {
+            Circle()
+                .frame(width: 200, height: 200)
+                .foregroundColor(.red)
+                .overlay {
+                    Text("\(counter)")
+                        .font(.system(size: 100, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                }
+           
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        CounterView()
     }
 }
